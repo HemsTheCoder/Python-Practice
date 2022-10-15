0216ae302d8daba30d844777dc4d335acca72d03
Hemant <hemsthecoder@gmail.com>
2022-10-15 12:15:46 +0530

    Initial Commit

---
+++ 01_print poem.py
@@ -0,0 +1,7 @@
+# Here we will print multi line poem with the help of print function
+print("Here is my poem")
+print(''' Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+''')
---
+++ 02_module_for_fun.py
@@ -0,0 +1,5 @@
+
+from playsound import playsound
+print("playing song")
+playsound('D:\Python\Best Dj Beat.mp3') 
+ 
