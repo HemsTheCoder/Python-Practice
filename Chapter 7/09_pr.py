204e63d6b9fe3140c9683d64767587cf16585697
Hemant <hemsthecoder@gmail.com>
2022-10-20 20:15:33 +0530

    Final commit

--- Chapter 7/09_pr.py
+++ Chapter 7/09_pr.py
@@ -1,6 +1,6 @@
-n=3
-m=3
-for i in range(n):
-    m=m-1
-    print(" "*(m),end="")
-    print("*" * (2*i+1), end="")
+# here we will print the table in a reverse order.
+num=int(input("Enter a number:"))
+for i in range(10,0,-1):
+  
+    if i<=10:
+       print(num,"x",i,"=",num*i)
